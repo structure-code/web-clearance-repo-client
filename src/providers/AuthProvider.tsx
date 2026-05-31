82c537ebbb4aa4b94fcdcf4d92656ae7f7604b98
Ale Busayo Josiah <114235176+busyboy090@users.noreply.github.com>
2026-05-31 16:40:38 +0100

    Update AuthProvider.tsx

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -25,6 +25,8 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         setUser(data);
       } catch (error: any) {
           setUser(null)
+      }finally {
+        setIsLoading(false)
       }
     };
 
